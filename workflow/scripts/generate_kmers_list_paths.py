0a6448c3883084133674703795b7909fd21c92c4
Kivanc Corut <kivanc.corut@uga.edu>
2021-11-09 10:51:11 -0500

    Reorder output based on samples table

--- workflow/scripts/generate_kmers_list_paths.py
+++ workflow/scripts/generate_kmers_list_paths.py
@@ -5,20 +5,37 @@ import numpy as np
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-i', '--in_dir', help='', type=str, required=True)
+parser.add_argument('-s', '--samp_tab', help='', type=str, required=True)
 parser.add_argument('-o', '--out_dir', help='', type=str, required=True)
 args = parser.parse_args()
 
 dir_names = os.listdir(args.in_dir)
 
+## Read samples.tsv (provided by the user) file 
+# to get the order of samples
+samples_tab = pd.read_csv(args.samp_tab, 
+                                sep="\t",
+                                usecols=["sample_name"],
+                                dtype='object')
+
+## Loop through kmers_count results for each
+# accession/sample directory and make a list of 
+# kmers_with_strand file paths for each accession/sample
 input_list=[]
 dirs=[]
 for dir in dir_names:
     if os.path.isfile(args.in_dir + '/' + dir + '/' + "kmers_with_strand"):
         input_list.append(args.in_dir + '/' + dir + '/' + "kmers_with_strand")
         dirs.append(dir)
-print(dirs)
 
-df = pd.DataFrame(input_list,columns=['input_files'])
-df["accession"] = dirs
-df.to_csv(args.out_dir + '/' + "kmers_list_paths.txt", index=False, header=False, sep="\t")
-print(input_list)
+## Make a dataframe with columns accession name and 
+# kmers_with_strand file path for that acession
+list_paths = pd.DataFrame(input_list,columns=['input_files'])
+list_paths["accession"] = dirs
+
+## Make sure the output file has same sample order as in samples.tsv file
+list_paths_sorted = samples_tab.merge(list_paths, left_on='sample_name',
+          right_on = 'accession').drop('sample_name',axis=1)
+
+## Write out output file
+list_paths_sorted.to_csv(args.out_dir + '/' + "kmers_list_paths.txt", index=False, header=False, sep="\t")
