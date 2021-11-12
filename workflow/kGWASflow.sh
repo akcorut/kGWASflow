322f389bd5e0448df17475cf94350caa06ed1067
Kivanc Corut <kivanc.corut@uga.edu>
2021-11-12 12:33:28 -0500

    Minor changes

--- workflow/kGWASflow.sh
+++ workflow/kGWASflow.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --partition=highmem_p
+#SBATCH --partition=wallace_p
 #SBATCH --job-name=kGWASflow
 #SBATCH --ntasks=1
 #SBATCH --time=168:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=30
-#SBATCH --mem=500gb
+#SBATCH --mem=250gb
 
 #SBATCH --mail-user=ac32082@uga.edu
 #SBATCH --mail-type=END,FAIL
@@ -23,4 +23,5 @@ source activate kGWASflow
 export LC_ALL=en_SG.utf8
 export LANG=en_SG.utf8
 
-snakemake --report report.html --reason --use-conda --conda-frontend conda --verbose --cores 30 --rerun-incomplete -s Snakefile
+snakemake --reason --use-conda --conda-frontend conda --verbose --cores 30 --rerun-incomplete -s Snakefile
+# snakemake -n --report report.html --reason --use-conda --conda-frontend conda --verbose --cores 30 --rerun-incomplete -s Snakefile
