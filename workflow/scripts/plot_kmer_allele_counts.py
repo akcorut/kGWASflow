c7cdf87ff1246b8cc3374a8785afdeab987b7e4c
Kivanc Corut <kivanc.corut@uga.edu>
2023-04-16 17:17:25 -0400

    fix: change x and y-axis ticks and labels

--- workflow/scripts/plot_kmer_allele_counts.py
+++ workflow/scripts/plot_kmer_allele_counts.py
@@ -2,15 +2,14 @@ import csv
 import os, glob, shutil
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy as sp
 from scipy import stats
 import matplotlib.ticker as ticker
+import matplotlib.pyplot as plt
 from matplotlib.pyplot import hist
 
-
-# logging
+# Logging
 with open(snakemake.log[0], "w") as f:
     sys.stderr = sys.stdout = f
 
@@ -20,21 +19,37 @@ with open(snakemake.log[0], "w") as f:
     # More info: https://github.com/voichek/kmersGWAS/blob/master/manual.pdf
     kmers_shareness = pd.read_csv(snakemake.input.shareness, 
                                     sep="\t", header=None, skiprows=1)
+    print(kmers_shareness)
 
     ## Plot the stats from kmers_to_use.shareness file
     # x-axis is allele count and y-axis is no. of k-mers
     # For every n accession/sample, the number of k-mers 
     # appeared in exactly that number of accessions.
-    fig_dims = (10, 10)
+    fig_dims = (10, 12)
     fig, ax = plt.subplots(figsize=fig_dims)
-    sns_plot = sns.barplot(x=kmers_shareness[0], y=kmers_shareness[1], color="darksalmon", saturation=.5,)
-    sns_plot.set_xlabel("Allele count",fontsize=18, weight='bold')
-    sns_plot.set_ylabel("#k-mers",fontsize=18, weight='bold')
+    sns_plot = sns.barplot(x=kmers_shareness[0], y=kmers_shareness[1], 
+                           color="darksalmon", 
+                           saturation=1)
+    sns_plot.set_xlabel("No. of samples",fontsize=14)
+    sns_plot.set_ylabel("No. of k-mers",fontsize=14)
+
     sns_plot.set_yscale("log")
+
+    # set y-axis ticks
+    yticks = [10**i for i in range(4, 10)]
+    sns_plot.yaxis.set_major_locator(ticker.FixedLocator(yticks))
+
+    # set y-axis tick labels
+    ytick_labels = [f"$10^{i}$" for i in range(4, 10)]
+    sns_plot.set_yticklabels(ytick_labels)
+
+    # set x-axis ticks
     sns_plot.xaxis.set_major_formatter(ticker.FormatStrFormatter('%d'))
-    sns_plot.xaxis.set_major_locator(ticker.MultipleLocator(base=5))
+    sns_plot.xaxis.set_major_locator(ticker.MultipleLocator(base=20))
+    
+    # rotate x axis labels
     ax.tick_params(axis='x', rotation=45)
     fig.tight_layout()
 
     ## Save the plot
-    sns_plot.figure.savefig(snakemake.output.kmer_allele_counts_plot, dpi=600)
+    sns_plot.figure.savefig(snakemake.output.kmer_allele_counts_plot, dpi=300)
