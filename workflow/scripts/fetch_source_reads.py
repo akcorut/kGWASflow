da92bdbb749ab7b841cd87fe6ba68f45627d2a3e
Kivanc Corut <kivanc.corut@uga.edu>
2023-04-16 17:36:44 -0400

    feat: add error handling
    * make sure that the output file is generated

--- workflow/scripts/fetch_source_reads.py
+++ workflow/scripts/fetch_source_reads.py
@@ -14,23 +14,23 @@ with open(snakemake.log[0], "w") as f:
                 ####### Load Snakemake Inputs and Params ########
                 #################################################
     
-    fq1_list= list(snakemake.input.r1)
-    fq2_list= list(snakemake.input.r2)
-    samp_list = list(snakemake.params.samples)
-    lib_list = list(snakemake.params.library)
+    fq1_list= list(snakemake.input.r1) # list of fastq r1 files
+    fq2_list= list(snakemake.input.r2) # list of fastq r2 files
+    samp_list = list(snakemake.params.samples) # list of sample names
+    lib_list = list(snakemake.params.library) # list of library names
     
-    kmers_tab = snakemake.input.kmers_tab
-    print(kmers_tab)
-    kmers_list_fa = snakemake.input.kmers_list
-    print(kmers_list_fa)
-    pheno = snakemake.params.pheno
-    print(pheno)
-    out_dir = snakemake.params.out_prefix
-    print(out_dir)
-    kmer_len = snakemake.params.kmer_len
-    print(kmer_len)
-    fetch_reads = snakemake.input.fetch_reads
-    print(fetch_reads)
+    kmers_tab = snakemake.input.kmers_tab # kmers table
+
+    kmers_list_fa = snakemake.input.kmers_list # kmers list fasta
+
+    pheno = snakemake.params.pheno # phenotype name
+
+    out_dir = snakemake.output.out_dir # output directory
+
+    kmer_len = snakemake.params.kmer_len # kmer length
+
+    fetch_reads = snakemake.input.fetch_reads # fetch_reads script
+
         
                     #########################################
                     ####### Generate a Samples Table ########
@@ -43,9 +43,9 @@ with open(snakemake.log[0], "w") as f:
 
     # reads_path_tab.to_csv(snakemake.params.out_prefix + "test.txt", index=False, sep="\t")
 
-            ##############################################################
-            ####### Identfiy accessions having the k-mers present ########
-            ##############################################################
+            ###############################################################
+            ####### Identify the samples having the k-mers present ########
+            ###############################################################
 
     ## Read "kmers_table.txt" for a given phenotype
     kmers_table = pd.read_csv(kmers_tab, 
@@ -71,26 +71,43 @@ with open(snakemake.log[0], "w") as f:
     reads_path_tab_filt = reads_path_tab_filt.reset_index(drop=True)
 
     ## Check if output directory is exist. If not create one
-    if not os.path.exists(out_dir + '/' + pheno):
-        os.makedirs(out_dir + '/' + pheno)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
     
     ## Filter the reads that have one of the k-mers in them
     for index, row in reads_path_tab_filt.iterrows():
         if row["sample_name"] == row["library_name"]:
 
             ## Source code: https://github.com/voichek/fetch_reads_with_kmers
-            subprocess.run(" {fetch_reads} {r1} {r2} {fa} {kmer_len} {out}/{pheno}/{acc}_reads_with_kmers".format(
+            subprocess.run(" {fetch_reads} {r1} {r2} {fa} {kmer_len} {out}/{acc}_reads_with_kmers".format(
                 fetch_reads= fetch_reads, out=out_dir, 
                 r1= row["fq1"], r2= row["fq2"],
                 acc= row["sample_name"], lib = row["library_name"], 
                 fa= kmers_list_fa, pheno= pheno, kmer_len= kmer_len), shell=True)
+            
+            # make sure that the output file is generated and if not exit the script with error
+            if not os.path.exists("{out}/{acc}_reads_with_kmers_R1.fastq".format(out=out_dir, acc=row["sample_name"])):
+                print("Error: {acc}_reads_with_kmers_R1.fastq is not generated!".format(acc=row["sample_name"]))
+                sys.exit(1)
+            if not os.path.exists("{out}/{acc}_reads_with_kmers_R2.fastq".format(out=out_dir, acc=row["sample_name"])):
+                print("Error: {acc}_reads_with_kmers_R2.fastq is not generated!".format(acc=row["sample_name"]))
+                sys.exit(1)
+                
         else:
             ## Source code: https://github.com/voichek/fetch_reads_with_kmers
-            subprocess.run(" {fetch_reads} {r1} {r2} {fa} {kmer_len} {out}/{pheno}/{acc}_{lib}_reads_with_kmers".format(
+            subprocess.run(" {fetch_reads} {r1} {r2} {fa} {kmer_len} {out}/{acc}_{lib}_reads_with_kmers".format(
                 fetch_reads= fetch_reads, out=out_dir, 
                 r1= row["fq1"], r2= row["fq2"],
                 acc= row["sample_name"], lib = row["library_name"], 
                 fa= kmers_list_fa, pheno= pheno, kmer_len= kmer_len), shell=True)
+            
+             # make sure that the output file is generated and if not exit the script with error
+            if not os.path.exists("{out}/{acc}_{lib}_reads_with_kmers_R1.fastq".format(out=out_dir, acc=row["sample_name"], lib=row["library_name"])): 
+                print("Error: {acc}_{lib}_reads_with_kmers_R1.fastq is not generated!".format(acc=row["sample_name"], lib=row["library_name"]))
+                sys.exit(1)
+            if not os.path.exists("{out}/{acc}_{lib}_reads_with_kmers_R2.fastq".format(out=out_dir, acc=row["sample_name"], lib=row["library_name"])):
+                print("Error: {acc}_{lib}_reads_with_kmers_R2.fastq is not generated!".format(acc=row["sample_name"], lib=row["library_name"]))
+                sys.exit(1)
             
             
     
